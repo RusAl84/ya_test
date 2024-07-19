4bfba9974ec5bd8d86fbfd2655ab8a730303dfb2
dunno <116356493+dunnotoru@users.noreply.github.com>
2024-07-19 23:24:36 +0700

    update run.py add patronymic

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-def print_my_info(name: str, surname: str) -> None:
-    print(f'Меня зовут {name} {surname}')
+def print_my_info(name: str, patronymic: str) -> None:
+    print(f'Меня зовут {name} {patronymic}')
 
 
-print_my_info('Alexander', 'Kolesov')
+print_my_info('Alexander', 'Aleksandrovich')
